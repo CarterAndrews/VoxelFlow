aa701788fb04c3ef46cb30f456bc66c3c206de34
Carter <andrewscart@gmail.com>
2020-06-05 16:06:16 -0700

    init

---
+++ Assets/scripts/BlockPlacer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class BlockPlacer : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject SolidPrefab;
+    public GameObject FluidPrefab;
+
+    public  Type placing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        placing=typeof(Solid);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
+            placing=typeof(Solid);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha2)){
+            placing=typeof(Fluid);
+        }
+        if(Input.GetMouseButtonDown(0)){
+            Ray ray=cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit)){
+                Vector3Int pos=Vector3Int.RoundToInt(hit.collider.transform.position+hit.normal);
+                if(placing==typeof(Solid)){
+                    GameObject gm=Instantiate(SolidPrefab,pos,SolidPrefab.transform.rotation);
+                    FluidManager.addBlock(pos,gm.GetComponent<Solid>());
+                }
+                else{
+                    GameObject gm=Instantiate(FluidPrefab,pos,SolidPrefab.transform.rotation);
+                    FluidManager.addBlock(pos,gm.GetComponent<Fluid>());
+                }
+            }
+        }
+    }
+}
---
+++ Assets/scripts/FluidManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FluidManager : MonoBehaviour
+{
+    public static Block[,,] blocks;
+    public static List<Fluid> fluids;
+    // Start is called before the first frame update
+    void Start()
+    {
+        blocks=new Block[1000,1000,1000];
+        for(int i=0;i<1000;i++){
+            for(int j=0;j<1000;j++){
+                for(int k=0;k<1000;k++){
+                    blocks[i,j,k]=null;
+                }
+            }
+        }
+    }
+    public static void addBlock(Vector3Int pos,Block block){
+        blocks[pos.x,pos.y,pos.z]=block;
+        if(block is Fluid){
+            fluids.Add((Fluid) block);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
